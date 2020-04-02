d0c0cb565f372d681046a0f42afffae52618aa60
avarvind77 <33726557+avarvind77@users.noreply.github.com>
2020-04-02 16:12:11 +0530

    initial commit

---
+++ src/components/header/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import imgSrc from "../../images/picture.jpg";
+import Socials from "../socials";
+
+import classes from "./header.module.css";
+
+// import useSiteMetadata from '../../hooks/use-site-metadata'
+
+const Header = () => {
+
+  // const { siteURL } = useSiteMetadata();
+  
+  return (
+    <div className={classes.headerWrapper}>
+      <div className={classes.profileWrapper}>
+        <img
+          className={classes.profilePicture}
+          src={`${imgSrc}`}
+          alt="Arvind Pradhan"
+        />
+      </div>
+      <div className={classes.profileContent}>
+        <h1>Arvind Pradhan</h1>
+        <h2>Android Developer based in Bangalore, India.</h2>
+        <Socials />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,67 @@
+import React from "react"
+
+import Header from "../components/header"
+import Layout from "../components/layout";
+import Link from "../components/link";
+import Section from "../components/section";
+import SEO from "../components/seo";
+
+const IndexPage = () => (
+  <Layout>
+    <Header />
+    <SEO title="Akash Vishwakarma" />
+    <Section id="about" title="About Me">
+      <p>
+        I'm a motivated full-stack engineer who has experience building and
+        scaling web applications and services in various domains.
+      </p>
+      <p>
+        I'm also passionate about open source contributions, working in high
+        impact environments with frequent deployments, and helping growing
+        development teams build great products.
+      </p>
+    </Section>
+    <Section id="experience" title="Experience">
+      <Link
+        title="Milliman India"
+        link="https://us.milliman.com/"
+        desc="Associate Programmer, June 2017 - Present"
+      />
+    </Section>
+    <Section id="projects" title="Projects">
+      <Link
+        title="NPM @types/happypack"
+        link="https://www.npmjs.com/package/@types/happypack"
+        desc="Contributed to DefinitelyTyped"
+      />
+      <Link
+        title="CodeWithAkash"
+        link="https://www.youtube.com/channel/UCVlpi6qEkrdJpT1qDFmbbtQ"
+        desc="A YouTube Channel"
+      />
+      <Link
+        title="Personal Blog"
+        link="https://akashishu777.github.io/CodeWithAkash/"
+        desc="An open source and customizable single-page personal Blog template for developers"
+      />
+      <Link
+        title="Feedback Application"
+        link="https://github.com/akashishu777/FeedbackApplication"
+        desc="A simple restful NodeJS Feedback Application, with React, Redux, expressJS and mongoDB."
+      />
+    </Section>
+    <Section id="skills" title="Skills">
+      <Link
+        title="Languages & Frameworks"
+        desc="JavaScript (ES6+), React, Redux, Node.js, TypeScript, Webpack, C#, Python, F#"
+      />
+      <Link title="Databases" desc="Microsoft SQL Server, MongoDB" />
+      <Link
+        title="Other"
+        desc="Azure, TeamCity, GitHub, SVN, CI / CD, API design, Agile / Scrum"
+      />
+    </Section>
+  </Layout>
+);
+
+export default IndexPage
